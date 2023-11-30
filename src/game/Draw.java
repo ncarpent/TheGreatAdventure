0d60be06f4896e64d84f9866fb152ac242c48bfa
Kenza Mohammedi <kenza.mohammedi@edu.univ-eiffel.fr>
2023-11-30 23:13:20 +0100

    ajout du dossier Elements qui contient toutes les images en png + chargement des image dans Draw.java

--- src/game/Draw.java
+++ src/game/Draw.java
@@ -2,22 +2,59 @@ package game;
 
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.nio.file.Files;
 import java.util.EnumMap;
 import java.util.Objects;
+
+import javax.imageio.ImageIO;
 public class Draw {
 	private final Double ELEMENTPROPORTION = 1 / 10.0;
 	private final Double XCELL = width * ELEMENTPROPORTION;
 	private final Double YCELL = heigth * ELEMENTPROPORTION;
 	private static final EnumMap<ObstacleType, Image> obstaclesImages = new EnumMap<>(ObstacleType.class);
-	private static final EnumMap<DecoType, Image> obstaclesImages = new EnumMap<>(DecoType.class); 
-	private static final EnumMap<WeaponType, Image> obstaclesImages = new EnumMap<>(WeaponType.class);
+	private static final EnumMap<DecoType, Image> decoImages = new EnumMap<>(DecoType.class); 
+	private static final EnumMap<WeaponType, Image> weaponsImages = new EnumMap<>(WeaponType.class);
 	
-	public void drawElements(Graphics2D graphics, GameMap map){
-		
+	private void loadObstacles() {
+		for(var key : ObstacleType.class.getEnumConstants()){
+			   var path = Path.of(("Elements").resolve("obstacle"), key.toString() + ".png");
+				try(var input = Files.newInputStream(path)) {
+					obstaclesImages.put(key, ImageIO.read(input));
+				}
+		}
+	}
+	private void loadDecorations() {
+		for(var key : DecoType.class.getEnumConstants()){
+			   var path = Path.of(("Elements").resolve("decoration"), key.toString() + ".png");
+				try(var input = Files.newInputStream(path)) {
+					decoImages.put(key, ImageIO.read(input));
+				}
+		}
+	}
+	private void loadWeapons() {
+		for(var key : WeaponType.class.getEnumConstants()){
+			   var path = Path.of(("Elements").resolve("weapons"), key.toString() + ".png");
+				try(var input = Files.newInputStream(path)) {
+					weaponsImages.put(key, ImageIO.read(input));
+				}
+		}
 	}
 	
-	public void draw(Graphics2D graphics, Element element){
+	public void loadImages() {
+	   loadObstacles();
+	   loadDecorations();
+	   loadWeapons();
+	}
+	
+	public void drawElements(Graphics2D graphics, List<Element> listElements){
+		Objects.requireNonNull(listElements);
+		Objects.requireNonNull(graphics);
 		
+		for(var element : listElements) {
+			draw(graphics, element);
+		}
+	}
+	private static void draw(Graphics2D graphics, Element element){
 		Objects.requireNonNull(element);
 		Objects.requireNonNull(graphics);
 		switch(element){
@@ -33,21 +70,21 @@ public class Draw {
 		Objects.requireNonNull(graphics);
 		
 		var image = obstaclesImages.get(obstacle.obstacle());
-		graphics.drawImage(image, obstacle.x() * 24, obstacle.y() * 24, null);
+		graphics.drawImage(image, obstacle.x() * XCELL, obstacle.y() * YCELL, null);
 	}
 
 	private static void drawDecoration(Graphics2D graphics, Decoration decoration) {
 		Objects.requireNonNull(decoration);
 		Objects.requireNonNull(graphics);
 		var image = obstaclesImages.get(decoration.decoration());
-		graphics.drawImage(image, decoration.x() *24, decoration.y() * 24, null);
+		graphics.drawImage(image, decoration.x() * XCELL, decoration.y() * YCELL, null);
 	}
 
 	private static void drawWeapon(Graphics2D graphics, Weapon weapon) {
 		Objects.requireNonNull(weapon);
 		Objects.requireNonNull(graphics);
 		var image = obstaclesImages.get(weapon.decoration());
-		graphics.drawImage(image, weapon.x() *24, weapon.y() * 24, null);
+		graphics.drawImage(image, weapon.x() *XCELL, weapon.y() * YCELL, null);
 	}
 	
 	
